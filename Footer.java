c5cd2d78929efabd222626e4e2f60136c5aad410
Mohammad Abdullah <mohammadabdullah2@mail.adelphi.edu>
2026-09-17 09:30:38 -0400

    Enhance Footer.java with comments
    Added comments to improve code clarity.

--- Footer.java
+++ Footer.java
@@ -4,13 +4,17 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+// Adds a footer containing a logo and three buttons
 public class Footer {
+
+    // Footer components
     private JPanel footerPanel;
     private JLabel logoLabel;
     private JButton clearBtn;
     private JButton submitBtn;
     private JButton exitBtn;
 
+    // Initializes footer omponents
     public Footer() {
         footerPanel = new JPanel(
             new FlowLayout(FlowLayout.CENTER, 10, 10)
@@ -23,6 +27,7 @@ public class Footer {
         submitBtn = new JButton("Submit");
         exitBtn = new JButton("Exit");
 
+        // Adds logo and buttons to footer panel
         footerPanel.add(logoLabel);
         footerPanel.add(clearBtn);
         footerPanel.add(submitBtn);
